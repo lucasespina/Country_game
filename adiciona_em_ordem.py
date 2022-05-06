0f43b24eb5cf9ff872e9418a055a150826d70609
Albert Hamoui <103454880+alberthamoui@users.noreply.github.com>
2022-05-06 11:45:02 -0300

    correcao

--- adiciona_em_ordem.py
+++ adiciona_em_ordem.py
@@ -20,7 +20,7 @@ def adiciona_em_ordem(pais, distancia, lista):
             else:
                 lista_ordenada.append(lista[i])
                 
-        i = i + 1
+        i += 1
         
     if pais_escolhido not in lista_ordenada:
         lista_ordenada.append(pais_escolhido)
@@ -29,3 +29,29 @@ def adiciona_em_ordem(pais, distancia, lista):
     return lista_ordenada
 
 
+# def adiciona_em_ordem(pais, distancia, lista):
+    
+#     pais_escolhido = [pais, distancia]
+#     lista_ordenada = []
+
+#     if len(lista) == 0:
+#         lista_ordenada.append(pais_escolhido)
+#         return lista_ordenada
+
+#     for i in range(len(lista)):
+        
+#         if lista[i][1] < pais_escolhido[1]:
+#             lista_ordenada.append(lista[i])
+    
+#         else:
+#             if pais_escolhido not in lista_ordenada:
+#                 lista_ordenada.append(pais_escolhido)
+#                 i = i - 1
+#             else:
+#                 lista_ordenada.append(lista[i])
+        
+#     if pais_escolhido not in lista_ordenada:
+#         lista_ordenada.append(pais_escolhido)
+        
+
+#     return lista_ordenada
