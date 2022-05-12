7958d1308f6d2c4e37c06edaf3e633d33298bfe4
Lucas Espina <lucaseqc@al.insper.edu.br>
2022-05-12 19:14:29 -0300

    Reformulação

--- esta_na_lista.py
+++
@@ -1,6 +0,0 @@
-def esta_na_lista(pais, lista):
-    for lugar in lista:
-        if pais == lugar[0]:
-            return True
-    else:
-        return False
--- normaliza.py
+++
@@ -1,8 +0,0 @@
-def normaliza(dados):
-    dsd = dict()
-    for cont in dados:
-        for pais in dados[cont]:
-            
-            dsd[pais] = dados[cont][pais]
-            dsd[pais]['continente'] = cont
-    return dsd
